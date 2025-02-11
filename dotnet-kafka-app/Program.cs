029029e5a59d5c7210866c65275d0a141b386c81
pkredhat <phillip.knezevich@gmail.com>
2025-02-11 15:28:32 -0500

    Update Program.cs
    swapped methods

--- dotnet-kafka-app/Program.cs
+++ dotnet-kafka-app/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 Console.WriteLine("Starting Kafka Consumer...");
 
-// Call Kafka consumer logic (from a class)
+// Call Kafka consumer logic (from a class) FIX THIS
 Consumer.Run();
 //Producer.Run();
 
